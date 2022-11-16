f1235fd92d704bf822414fb6e2bf7ce8bc9e1fce
Sasak1tsune <juanpapereass@gmail.com>
2022-11-16 12:22:32 +0100

    dinero

---
+++ U3_R4Ejer3_20_JuanPablo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class U3_R4Ejer3_20_JuanPablo{
+    public static void main(String[]args){
+    //Defino variables
+    Scanner entrada = new Scanner(System.in);
+    float dinero, sum_dinero=0;
+
+
+    System.out.println("Introduce la cantidad de dinero con la que cuentas\nNo importa donde lo tengas, todo cuenta");
+    
+
+    }
+}
