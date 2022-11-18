621f8655dd29cd067fc61ba2795d8c061563c031
Sasak1tsune <juanpapereass@gmail.com>
2022-11-18 10:08:38 +0100

    Ejercicio 18/11/2022

--- U3_R4Ejer3_20_JuanPablo.java
+++ U3_R4Ejer3_20_JuanPablo.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
-public class U3_R4Ejer3_20_JuanPablo{
-    public static void main(String[]args){
-    //Defino variables
-    Scanner entrada = new Scanner(System.in);
-    float dinero, sum_dinero=0;
+public class U3_R4Ejer3_20_JuanPablo {
+    public static void main(String[] args) {
+        //Defino variables
+        Scanner entrada = new Scanner(System.in);
+        float dinero, sum_dinero = 0;
 
 
-    System.out.println("Introduce la cantidad de dinero de la siguiente forma");
-    while(dinero!=0){
+        System.out.println("Introduce la cantidad de dinero que tengas en los bolsillos, banco, cajones, lo que sea\nPara salir del programa introduce ''0''");
+        do {
+            dinero = entrada.nextFloat();
+            sum_dinero = sum_dinero + dinero;
+            if (dinero != 0) {
+                System.out.println("Algo mas?");
+            }
+        } while (dinero != 0);
 
-    }
-
+        System.out.println("La cantidad de dinero total es: " + sum_dinero);
+        if (sum_dinero == 0) {
+            System.out.println("Tiempos dificiles, eh?");
+        }
     }
 }
